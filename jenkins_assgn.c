69111333dca8ae37da9bf97409d754d5271b1cf3
Ketaki Jadhav <67627819+Ketaki-k-jadhav@users.noreply.github.com>
2022-11-10 00:30:59 +0530

    added another function

--- jenkins_assgn.c
+++ jenkins_assgn.c
@@ -4,11 +4,21 @@ int multiply(int a, int b){
     return a*b;
 }
 
+int diff(int a,int b){
+    if(a>b)
+        return a-b;
+    else
+        return b-a
+}
+
 int main(){
     int x=67;
     int y=29;
 
     int res1=multiply(x,y);
     printf("%d * %d : %d\n",x,y,res1);
+    
+    int res2=diff(x,y);
+     printf("Difference between %d and %d : %d\n",x,y,res2);
 
 }
